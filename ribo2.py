443854c00874fe58070af5a2df6c535a803f579a
Keita Sasaki <noko@net45-dhcp202.sfc.keio.ac.jp>
2016-05-24 17:48:30 +0900

    ribosomeモデルと結合。r_uをr30とr50で生成

--- ribo2.py
+++ ribo2.py
@@ -6,23 +6,39 @@ from ecell4 import *
 util.decorator.SEAMLESS_RATELAW_SUPPORT = True
 
 """
-$B%j%\%=!<%`$NF0BV(B
+リボソームの動態
 r50, r30, r
 """
 
-def createModel():
-    Ka = 0.01
-    Kd = 0.3
+def createModel(r_max=65.8, r_min=19.3, K_D=1.0, K_t=6.1*10**-2, K_on=3.0, Lambda_0=1.35, Lambda_0_a=0.31, IC50=0.41, IC50_a=0.189):
+    Delta_r = r_max - r_min # µM
+    K_off = K_on * K_D
+    P_in = Delta_r * Lambda_0_a / 2.0 / IC50_a 
+    P_out = (Lambda_0_a / 2) ** 2.0 / K_t / K_D
+
     with reaction_rules():
-        r30_u + r50_u == r_u | (Ka, Kd) # ribo$B$N9g@.(B
+        Lambda = (r_u - r_min) * K_t
+        Ka = Lambda * (r_max - Lambda * Delta_r * (1 / Lambda_0 - 1/K_t / Delta_r))
+        Kd = r_u * Lambda
+
+        ~a_ex > a | P_in * a_ex
+        a > ~a_ex | P_out * a
+        a + r_u > r_b | K_on * a * (r_u - r_min)
+        r_b > a + r_u | K_off * r_b
+        # ~r_u > r_u | Lambda * (r_max - Lambda * Delta_r * (1 / Lambda_0 - 1/K_t / Delta_r))
+        # r_u > ~r_u | r_u * Lambda
+        a > ~a | a * Lambda
+        r_b > ~r_b | r_b * Lambda
+        r30_u + r50_u == r_u | (Ka, Kd) # riboの合成
 
     return get_model()
 
-def run(step=10., legend=[], dataset={}):
-    model = createModel()
-    y0 = {"r30_u":60., "r50_u":60., "r_u": 0.}
-    if dataset:
-        y0.update(dataset)
+def run(a_ex, step=10., legend=[], inpData={}, y0={"r30_u":60., "r50_u":60., "r_u": 30.0, "a": .0, "r_b": .0}):
+    dataset = {"Lambda_0": 1.35, "Lambda_0_a": 0.31, "IC50": 0.41, "IC50_a": 0.189, "K_t": 6.1 * 10 ** -2, "r_min": 19.3}
+    
+    dataset.update(inpData)
+    model = createModel(**dataset)
+    y0["a_ex"] = a_ex
 
     if not legend:
         legend = y0.keys()
@@ -39,10 +55,11 @@ def makeGraph(data, legend=[]):
             plt.plot(data.T[0], data.T[i+1], label=legend[i])
         else:
             plt.plot(data.T[0], data.T[i+1])
-    # plt.savefig("result/test.png", dpi=200)
     plt.legend(loc="upper right")
     plt.show()
+    # plt.savefig("result/ribo2_2_3.png", dpi=200)
 
 if __name__ == "__main__":
-    result, legend = run()
+    dataset = {"Lambda_0": 0.982371812727}
+    result, legend = run(.5, inpData=dataset)
     makeGraph(np.array(result), legend)
