f27346e002b2a7429ba8c30d1e5f063e69b1d338
Keita Sasaki <noko@sasakikeita-no-MacBook-Air.local>
2016-05-27 17:13:29 +0900

    r30, r50の拡散、増加の追加。Kawo

--- ribo2.py
+++ ribo2.py
@@ -10,26 +10,59 @@ util.decorator.SEAMLESS_RATELAW_SUPPORT = True
 r50, r30, r
 """
 
-def createModel(r_max=65.8, r_min=19.3, K_D=1.0, K_t=6.1*10**-2, K_on=3.0, Lambda_0=1.35, Lambda_0_a=0.31, IC50=0.41, IC50_a=0.189):
+def createModel(r_max=65.8, r_min=19.3, K_D=1.0, K_t=6.1*10**-2, K_on=3.0, Lambda_0=1.35, Lambda_0_a=0.31, IC50=0.41, IC50_a=0.189, Ka=3., Kd=.0):
+    """
+    リボソームモデルを構成するモジュール
+    r_max: µM
+    r_min: µM
+    K_D: none
+    K_t: 1/µM/h
+    K_on: ???
+    K_off: ???
+    Delta_r: µM
+    Lambda_0: default medium = Gly_RDM
+    Lambda_0_a: default drug = streptmycin
+    IC50: default medium = Gly_RDM, default drug = streptmycin
+    IC50_a: default drug = streptmycin
+    Ka: subunitの結合
+    Kd: subunitの解離
+
+    薬剤によって変更する必要がある値:
+    Lambda_0_a, IC50, IC50_a
+
+    培地によって変更する値(COBRAの結果):
+    Lambda_0
+    """
+
     Delta_r = r_max - r_min # µM
     K_off = K_on * K_D
     P_in = Delta_r * Lambda_0_a / 2.0 / IC50_a 
     P_out = (Lambda_0_a / 2) ** 2.0 / K_t / K_D
 
     with reaction_rules():
+        ## expression
         Lambda = (r_u - r_min) * K_t
-        Ka = Lambda * (r_max - Lambda * Delta_r * (1 / Lambda_0 - 1/K_t / Delta_r))
-        Kd = r_u * Lambda
+        SUP = Lambda * (r_max - Lambda * Delta_r * (1 / Lambda_0 - 1/K_t / Delta_r)) # subunit production expression
 
+        ## reaction
+        # a
         ~a_ex > a | P_in * a_ex
         a > ~a_ex | P_out * a
+        a > ~a | a * Lambda
+        
+        # ribo and subunit
+        r30_u + r50_u > r_u | Ka * r30_u * r50_u
+        r_u > r30_u + r50_u | Kd * r_u 
+        ~r30_u > r30_u | sup # production
+        ~r50_u > r50_u | sup # production
+        r_u > ~r_u | r_u * Lambda # diffusion
+        r30_u > ~r30_u | r30_u * Lambda # diffusion
+        r50_u > ~r50_u | r50_u * Lambda # diffusion
+
+        # bond drug
         a + r_u > r_b | K_on * a * (r_u - r_min)
         r_b > a + r_u | K_off * r_b
-        # ~r_u > r_u | Lambda * (r_max - Lambda * Delta_r * (1 / Lambda_0 - 1/K_t / Delta_r))
-        # r_u > ~r_u | r_u * Lambda
-        a > ~a | a * Lambda
         r_b > ~r_b | r_b * Lambda
-        r30_u + r50_u == r_u | (Ka, Kd) # riboの合成
 
     return get_model()
 
@@ -49,17 +82,31 @@ def run(a_ex, step=10., legend=[], inpData={}, y0={"r30_u":60., "r50_u":60., "r_
     data = runsim.data()
     return data, legend
 
-def makeGraph(data, legend=[]):
+def makeGraph(data, savename, legend=[]):
     for i in range(len(data[0]) - 1):
         if legend[i]:
             plt.plot(data.T[0], data.T[i+1], label=legend[i])
         else:
             plt.plot(data.T[0], data.T[i+1])
     plt.legend(loc="upper right")
-    plt.show()
-    # plt.savefig("result/ribo2_2_3.png", dpi=200)
+    # plt.show()
+    plt.savefig("result/%s" % (savename), dpi=200)
+    plt.close()
 
 if __name__ == "__main__":
+    """
+    savename = "ribo2_1.png"
     dataset = {"Lambda_0": 0.982371812727}
-    result, legend = run(.5, inpData=dataset)
-    makeGraph(np.array(result), legend)
+    legend = ["r_u", "r_b", "a_ex", "a"]
+    result, legend = run(.0, inpData=dataset, legend=legend)
+    makeGraph(np.array(result), savename, legend)
+    """
+    count = 0
+    for i in np.linspace(0, 1, 101):
+        legend = ["r_u", "r_b", "a_ex", "a"]
+        dataset = {"Ka": i, "Lambda_0": 0.982371812727}
+        savename = "ribo2_%d.png" % (count)
+        result, legend = run(.0, inpData=dataset, legend=legend)
+        makeGraph(np.array(result), savename, legend)
+        print "Ka: %f finish." % (i)
+        count += 1
