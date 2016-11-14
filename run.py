1885d154f00b788bc7328df80036cebcc7e97286
KeitaS <noko@sfc.keio.ac.jp>
2016-11-14 16:43:52 +0900

    ribo5.py linechartにて確認

--- run.py
+++ run.py
@@ -1,26 +1,37 @@
-#coding:utf-8
-
+# coding: utf-8
+from ribo5 import *
+from modules import *
 import numpy as np
+import seaborn as sns
+import pandas as pd
 import matplotlib.pylab as plt
-from ecell4 import *
-util.decorator.SEAMLESS_RATELAW_SUPPORT = True
-from ribo4 import *
 
-# 保存用ディレクトリの作成
-savedir = "./images/result4"
+savedir = "images/ribo5"
 makedir(savedir)
 
-# 初期値
-r_min = 19.3 # Lambdaの計算で使用
-K_t = 6.1 * 10 ** -2 # Lambdaの計算で使用
-Kd = 6. # r30とr50の解離率
-p = .1 # r30とr50の比率
+a_ex = 0.7 * 2
 
-## drug data
-dNames = ["Streptmycin", "Kanamycin", "Tetracycline", "Chloramphenicol"]
-Lambda_0 =  [1.35, 0.85, 0.40] # 1/h (1.35, 0.85, 0.40)
-Lambda_0_a = {"Streptmycin": 0.31, "Kanamycin": 0.169, "Tetracycline": 5.24, "Chloramphenicol": 1.83} # 1/h
-IC50 = {"Streptmycin": [0.41, 0.28, 0.196], "Kanamycin": [0.246, 0.096, 0.065], "Tetracycline": [0.5, 0.6, 1.45], "Chloramphenicol": [2.85, 2.65, 5.7]} # µg/ml
-IC50_a = {"Streptmycin": 0.189, "Kanamycin": 0.05, "Tetracycline": 0.229, "Chloramphenicol": 2.49} # µg/ml
-a_ex = {"Streptmycin": 0.6, "Kanamycin": 0.5, "Tetracycline":2, "Chloramphenicol": 20}
-dataset = {"Kd": Kd, "Lambda_0": Lambda_0[0], "p": p}
+# create line chart
+## single
+drugs = [makeDrugDatas("Kanamycin")]
+doses = np.linspace(0, a_ex, 31)
+result_list = []
+for dose in doses:
+    drugs[0]["dose"] = dose
+    result, legend = run(drugs, step=100, legend=["r_u"])
+    result = calcGrowthrate(result[-1][1])
+    result_list.append([dose, result])
+
+## double
+drugs = [makeDrugDatas("Kanamycin"), makeDrugDatas("Kanamycin")]
+doses = np.linspace(0, a_ex / 2, 31)
+for index, dose in enumerate(doses):
+    drugs[0]["dose"] = dose
+    drugs[1]["dose"] = dose
+    result, legend = run(drugs, step=100, legend=["r_u"])
+    result = calcGrowthrate(result[-1][1])
+    result_list[index].append(result)
+result_list = np.array(result_list)
+plt.plot(result_list.T[0], result_list.T[1], label="single")
+plt.plot(result_list.T[0], result_list.T[2], label="double")
+plt.legend()
