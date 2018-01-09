214ba2497796e6a6e088e2b5ee40690af2494564
KeitaS <noko@sfc.keio.ac.jp>
2018-01-10 00:08:03 +0900

    xlabel, ylabelの追加

--- run_ribo6.py
+++ run_ribo6.py
@@ -9,7 +9,7 @@ import itertools as itr
 import sys
 import seaborn as sns
 import matplotlib.pylab as plt
-from ribo6_test import makedir
+from ribo6 import makedir
 
 ## liner chart
 csvdir = "results/ribo6/csv/liner"
@@ -23,6 +23,8 @@ Lambda_0 = normal["single"][0]
 plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(normal["single"]))), label="single")
 plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(normal["double"]))), label="double")
 plt.legend()
+plt.xlabel("Dose")
+plt.ylabel("Growth Rate")
 plt.savefig("{}/liner_pattern0.png".format(imgdir), dpi=300)
 plt.close()
 
@@ -31,6 +33,8 @@ Lambda_0 = change["single"][0]
 plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(change["single"]))), label="single")
 plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(change["double"]))), label="double")
 plt.legend()
+plt.xlabel("Dose")
+plt.ylabel("Growth Rate")
 plt.savefig("{}/liner_pattern1.png".format(imgdir), dpi=300)
 
 
