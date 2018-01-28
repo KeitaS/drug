e4c99a3c3c31e06e05db2991e097d0ad12c9675a
KeitaS <noko@sfc.keio.ac.jp>
2018-01-29 00:14:54 +0900

    S型とG型の両方の出力と，画像の保存ファイル名の変更

--- run_ribo6.py
+++ run_ribo6.py
@@ -13,31 +13,34 @@ from ribo6 import makedir
 
 ## liner chart
 csvdir = "results/ribo6/csv/liner"
-drugName = "Chloramphenicol"
+drugNames = ["Streptmycin", "Chloramphenicol"]
 imgdir = "results/ribo6/images"
 makedir(imgdir)
 
 ## normal
-normal = pd.read_csv("{}/{}_0.csv".format(csvdir, drugName))
-Lambda_0 = normal["single"][0]
-plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(normal["single"]))), label="single")
-plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(normal["double"]))), label="double")
-plt.legend()
-plt.xlabel("Dose", fontsize=20)
-plt.ylabel("Growth Rate", fontsize=20)
-plt.tight_layout()
-plt.savefig("{}/liner_pattern0.png".format(imgdir), dpi=300)
-plt.close()
+for drugName in drugNames:
+    normal = pd.read_csv("{}/{}_0.csv".format(csvdir, drugName))
+    Lambda_0 = normal["single"][0]
+    plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(normal["single"]))), label="single")
+    plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(normal["double"]))), label="double")
+    plt.legend()
+    plt.xlabel("Dose", fontsize=20)
+    plt.ylabel("Growth Rate", fontsize=20)
+    plt.tight_layout()
+    plt.savefig("{}/{}_normal.png".format(imgdir, drugName), dpi=300)
+    plt.close()
 
-change = pd.read_csv("{}/{}_1.csv".format(csvdir, drugName))
-Lambda_0 = change["single"][0]
-plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(change["single"]))), label="single")
-plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(change["double"]))), label="double")
-plt.legend()
-plt.xlabel("Dose", fontsize=20)
-plt.ylabel("Growth Rate", fontsize=20)
-plt.tight_layout()
-plt.savefig("{}/liner_pattern1.png".format(imgdir), dpi=300)
+## change
+for drugName in drugNames:
+    change = pd.read_csv("{}/{}_1.csv".format(csvdir, drugName))
+    Lambda_0 = change["single"][0]
+    plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(change["single"]))), label="single")
+    plt.plot(np.linspace(0, 100, 101), list(map(lambda x: x / Lambda_0, list(change["double"]))), label="double")
+    plt.legend()
+    plt.xlabel("Dose", fontsize=20)
+    plt.ylabel("Growth Rate", fontsize=20)
+    plt.tight_layout()
+    plt.savefig("{}/{}_change.png".format(imgdir, drugName), dpi=300)
 
 
 
